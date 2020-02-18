df807be9909a9c492c3bb6a7168b258959bf92b1
TruthSeekerBeast <truthseekerbeast@gmail.com>
2020-02-18 16:42:10 +0530

    Revert "moved channel_layers from settings.py to asgi.py"
    This reverts commit 7a33db68

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -6,19 +6,7 @@ defined in the ASGI_APPLICATION setting.
 import os
 import django
 from channels.routing import get_default_application
-from chat_app.settings import SECRET_KEY
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_app.settings")
 django.setup()
 application = get_default_application()
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-        },
-        "symmetric_encryption_keys": [SECRET_KEY],
-    },
-}
-ASGI_APPLICATION = 'chat_app.routing.application'
