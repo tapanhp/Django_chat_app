6cefd1637107391fdfe9a01c52685805b396b6c1
TruthSeekerBeast <truthseekerbeast@gmail.com>
2020-02-19 12:39:40 +0530

    changed settings for deployment

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
-import asgi_redis
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -101,11 +100,10 @@ else:
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "asgi_redis.RedisChannelLayer",
         "CONFIG": {
             "hosts": [('localhost', '6379')],
         },
-        "ROUTING": "chat_app.routing.channel_routing",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
     },
 }
 ASGI_APPLICATION = 'chat_app.routing.application'
