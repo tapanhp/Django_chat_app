48bba3b370eb8852472f8dff8d5f2015416e89d5
TruthSeekerBeast <truthseekerbeast@gmail.com>
2020-02-19 14:27:56 +0530

    added application in asgi.py

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -6,7 +6,9 @@ defined in the ASGI_APPLICATION setting.
 import os
 import django
 import channels.layers
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_app.settings")
 django.setup()
 channel_layer = channels.layers.get_channel_layer()
+application = get_default_application()
