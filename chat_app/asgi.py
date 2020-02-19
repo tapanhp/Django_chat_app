40b4c166eaf136d74cdeaa07590b89c3cb1d7e7e
TruthSeekerBeast <truthseekerbeast@gmail.com>
2020-02-19 11:35:10 +0530

    added django setup for deploy to heroku

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -4,7 +4,9 @@ defined in the ASGI_APPLICATION setting.
 """
 
 import os
+import django
 import channels.layers
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_app.settings")
+django.setup()
 channel_layer = channels.layers.get_channel_layer()
